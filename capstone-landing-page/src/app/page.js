fe48b1ebe186709a0b1ee176bbf17e0a442fb10f
c0d1ngkjy <108091177+c0d1ngkjy@users.noreply.github.com>
2024-03-25 14:09:06 +0900

    landing page cards section

--- capstone-landing-page/src/app/page.js
+++ capstone-landing-page/src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Button } from '@nextui-org/button';
 import { SparklesCore } from "../components/ui/sparkles";
+import { HoverEffect } from "../components/ui/card-hover-effect";
 
 export default function Home() {
   return (
@@ -61,6 +62,48 @@ export default function Home() {
         </div>
       </div>
 
+      <div className="max-w-5xl mx-auto px-8">
+        <HoverEffect items={projects} />
+      </div>
     </main>
   );
 }
+
+export const projects = [
+  {
+    title: "멤버 관리",
+    description:
+      "A technology company that builds economic infrastructure for the internet.",
+    link: "https://stripe.com",
+  },
+  {
+    title: "문의 관리",
+    description:
+      "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+    link: "https://netflix.com",
+  },
+  {
+    title: "가입 관리",
+    description:
+      "A multinational technology company that specializes in Internet-related services and products.",
+    link: "https://google.com",
+  },
+  {
+    title: "일정 관리",
+    description:
+      "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
+    link: "https://meta.com",
+  },
+  {
+    title: "과제 관리",
+    description:
+      "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+    link: "https://amazon.com",
+  },
+  {
+    title: "자동화 시스템",
+    description:
+      "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
+    link: "https://microsoft.com",
+  },
+];
