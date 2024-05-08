7a22fb772473078716e1ddc2fedde783887be07b
codingkjy <codingkjy28@gmail.com>
2024-05-08 22:42:39 +0900

    .

---
+++ capstone-console/src/pages/LoginPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="bg-primary text-white column justify-center q-pa-xl">
+    <div class="column content-start">
+      <div>NO.1</div>
+      <div>동아리 운영 관리 서비스</div>
+      <div>
+        <div>여기모여</div>
+        <img src="~assets/logo.svg" alt="">
+      </div>
+      <q-btn color="yellow">카카오 로그인</q-btn>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'LoginPage'
+});
+</script>
--- capstone-console/src/router/routes.js
+++ capstone-console/src/router/routes.js
@@ -1,6 +1,13 @@
 const routes = [
   {
     path: '/',
+    component: () => import('layouts/EmptyLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/LoginPage.vue') }
+    ]
+  },
+  {
+    path: '/console',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
