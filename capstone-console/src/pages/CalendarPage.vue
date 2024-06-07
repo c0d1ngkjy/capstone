80a85589206a6b2e10b308a9081b179a1be092aa
c0d1ngkjy <108091177+c0d1ngkjy@users.noreply.github.com>
2024-06-07 16:14:26 +0900

    .

--- capstone-console/src/pages/CalendarPage.vue
+++ capstone-console/src/pages/CalendarPage.vue
@@ -6,27 +6,34 @@
           :locale="locale" @input="updateSelectedDate" />
 
         <q-list class="col q-ml-md bg-blue-grey-1" style="border-radius: 12px;">
-          <q-item v-for="(todo, index) in todos[selectedDate]" :key="index" style="border-radius: 12px;">
-            <q-item-section>{{ todo }}</q-item-section>
-            <q-item-section side>
+          <div v-if="todos[selectedDate]">
+            <q-item class="q-py-md" v-for="(todo, index) in todos[selectedDate]" :key="index"
+              style="border-radius: 12px;">
+
+              <q-item-section>
+                <q-item-label>{{ todo.title }}</q-item-label>
+                <q-item-label caption>{{ todo.description }}</q-item-label>
+              </q-item-section>
+
               <q-btn size="sm" unelevated @click="removeTodo(selectedDate, index)" color="negative" icon="delete"
-                style="border-radius: 8px;" />
-            </q-item-section>
-          </q-item>
+                class="float-right q-ma-md" style="border-radius: 8px; height: 27px" />
+            </q-item>
+          </div>
         </q-list>
       </div>
 
       <div class="q-mt-md">
-        <div class="flex bg-white " style="border-radius: 12px;">
-          <q-input borderless v-model="newTodo" label="일정 추가하기" @keyup.enter="addTodo"
+        <div class="flex q-gutter-md bg-white " style="border-radius: 12px;">
+          <q-input borderless v-model="newTodo.title" label="일정 (필수)" @keyup.enter="addTodo"
             class="col bg-blue-grey-1 q-px-md" style="border-radius: 12px;">
-            <template v-slot:append>
-              <q-btn unelevated color="primary" label="추가" @click="addTodo" class="q-ml-sm q-px-md"
-                style="border-radius: 8px;" />
-            </template>
           </q-input>
-        </div>
 
+          <q-input borderless v-model="newTodo.description" label="일정 설명" @keyup.enter="addTodo"
+            class="col bg-blue-grey-1 q-px-md" style="border-radius: 12px;">
+          </q-input>
+          <q-btn unelevated color="primary" label="추가" @click="addTodo" class="q-ml-md q-px-xl"
+            style="border-radius: 8px;" />
+        </div>
       </div>
     </div>
   </q-page>
@@ -34,15 +41,18 @@
 
 <script setup>
 import { useQuasar } from 'quasar';
+import { api } from 'src/boot/axios';
+import { useUserStore } from 'src/stores/user-store';
 import { onMounted, ref, watch } from 'vue';
 
 defineOptions({
   name: 'CalendarPage'
 })
 const $q = useQuasar()
+const userStore = useUserStore();
 
 const selectedDate = ref('');
-const newTodo = ref('');
+const newTodo = ref({});
 const todos = ref({});
 
 watch(todos.value, () => console.log(todos.value))
@@ -74,19 +84,30 @@ const updateSelectedDate = () => {
 };
 
 const addTodo = () => {
-  if (!newTodo.value.trim()) {
+  if (!newTodo.value.title) {
     // $q.notify({
     //   message: '일정을 입력해주세요',
     //   color: 'red'
     // })
     return
   }
+  const data = {
+    ...newTodo.value,
+    clubId: userStore.currentClub.club_id,
+    date: selectedDate.value
+  }
+  console.log(data)
+  api.post('calendar/create', data).then((res)=>{
+    console.log(res)
+  })
+
   if (!todos.value[selectedDate.value]) {
     todos.value[selectedDate.value] = [];
   }
 
-  todos.value[selectedDate.value].push(newTodo.value.trim());
-  newTodo.value = '';
+  todos.value[selectedDate.value].push(newTodo.value);
+
+  newTodo.value = {};
 };
 
 const removeTodo = (date, index) => {
